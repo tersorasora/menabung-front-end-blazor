4344ca891fc0e7fc15806d2ca38e4c6014b1ed65
tersorasora <kentonelek45@gmail.com>
2025-10-13 10:04:29 +0700

    first ever commit fr

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using BlazorAppTest.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddScoped(IServiceProvider => new HttpClient { BaseAddress = new Uri("http://localhost:5223/api/") });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
